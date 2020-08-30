59aaa5aeebd693094c5dee3065f4990766f62a56
Alexey Titov <alexey.titov@jetbrains.com>
2020-08-30 21:28:40 +0300

    fixed typo

--- src/hotspot/share/runtime/thread.cpp
+++ src/hotspot/share/runtime/thread.cpp
@@ -4571,19 +4571,23 @@ void Threads::deoptimized_wrt_marked_nmethods() {
 jlong Threads::compile_total_time_ms() {
   jlong accumulator = 0;
   MutexLocker mu(Threads_lock);
+  static std::map<JavaThread*, jlong> thread_to_cputime;
+
   jlong current_thread_count = 0;
   for (JavaThread* i = _thread_list; i != NULL; i = i->next(), ++current_thread_count) {
     if (i->is_Compiler_thread()) {
       jlong time = os::thread_cpu_time((Thread *) i, true) / 1000 / 1000;
-      accumulator += time;
+      thread_to_cputime[i] = time;
     }
   }
 
   static jlong previous_thread_count = current_thread_count;
-  assert(current_thread_count >= previous_thread_count, "thread count must not be less");
+  printf("thread count must not be less %lu -> %lu\n", previous_thread_count, current_thread_count);
 
   static jlong prev_returned_value = accumulator;
-  assert(prev_returned_value <= accumulator, "compile_total_time_ms should be greater than prev returned value");
+  printf("compile_total_time_ms should be greater than prev returned value %lu %lu\n", prev_returned_value, accumulator);
+
+  previous_thread_count = current_thread_count;
   prev_returned_value = accumulator;
   return accumulator;
 }
